2075a1664a3338d9a4df6142068c35b52bc14dfa
Pedro Galvão <162392286+PeedroHG@users.noreply.github.com>
2024-03-05 22:27:51 -0300

    Projeto concluído
    Conclusão de projeto front-end de site responsivo

---
+++ index.js
@@ -0,0 +1,19 @@
+function Teste(tipo) {
+
+  let lista = null
+
+  if (tipo == 1) {
+    lista = document.getElementById("lista");
+  } else {
+    lista = document.getElementById("lista2");
+  }
+
+  const computedStyle = window.getComputedStyle(lista);
+  const displayValue = computedStyle.getPropertyValue("display");
+
+  if (displayValue === "none") {
+    lista.style.display = "block";
+  } else {
+    lista.style.display = "none";
+  }
+}
